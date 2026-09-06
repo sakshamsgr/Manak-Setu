9d5a092449e8786dc2a907e6f31c9f912ea271bb
Saksham Sagar <mastersakshamsgr@gmail.com>
2026-09-06 14:13:07 +0530

    1st push by saksham in main

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-b38717c4'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.0kk71gnltp4"
+    "revision": "0.2ot706n7lvo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
