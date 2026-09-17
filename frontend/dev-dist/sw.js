7144f3517cf271dc140a8619d5d30838e223ba91
Saksham Sagar <mastersakshamsgr@gmail.com>
2026-09-17 23:37:03 +0530

    Pushhhhh

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-b38717c4'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.p8rghj9koo"
+    "revision": "0.b4qqooh4vps"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
